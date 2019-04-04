cc20fa11965b4ee97d2bd16143265893d2a69f9d
florvimberg <florencia.vimberg@ing.austral.edu.ar>
2019-04-04 14:45:58 -0300

    user front added

--- client-service/src/app/app-routing.module.ts
+++ client-service/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductsComponent} from './components/products/products.component';
+import {UserComponent} from './components/user/user.component';
 
 const routes: Routes = [
-  {path: 'products', component: ProductsComponent}
+  {path: 'products', component: ProductsComponent},
+  {path: 'users', component: UserComponent}
 ];
 
 @NgModule({
